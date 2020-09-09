2c0cef1ea41b62c175155937f778c8dba67d93d8
danielchowyc <daniellchow@gmail.com>
2020-09-09 20:39:10 +0100

    added delete bookinstance feature

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -41,7 +41,7 @@ exports.bookinstance_create_get = function(req, res, next) {
     .exec(function (err, books) {
       if (err) { return next(err); }
       // Successful, so render.
-      res.render('bookinstance_form', {title: 'Create BookInstance', book_list: books});
+      res.render('bookinstance_form', {title: 'Create Book Instance', book_list: books});
     });
     
 };
@@ -97,12 +97,25 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+    BookInstance.findById(req.params.id)
+    .exec(function(err, result) {
+        if (err) {return next(err)}
+        if (result == null) {
+            res.redirect('/catalog/bookinstances')
+        }
+        // Successful, so render.
+        res.render('bookinstance_delete', { title: 'Delete Book Instance', bookinstance: result } );
+    })
 };
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+    BookInstance.findByIdAndDelete(req.body.bookinstanceid, function deleteinst(err) {
+        if (err) {return next(err)}
+        // success
+        res.redirect('/catalog/bookinstances')
+    });
+    
 };
 
 // Display BookInstance update form on GET.
